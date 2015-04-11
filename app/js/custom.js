3db6e5be1a128bb77d9f0ce069cdbc573a1d17a0
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2015-04-12 00:15:27 +0700

    fix issue on mobile device

--- app/js/custom.js
+++ app/js/custom.js
@@ -22,11 +22,11 @@ jQuery(document).ready(function($) {
 	});
 
 	// Owl carousel
-	$("#owl-demo").owlCarousel({
+	$("#owl").owlCarousel({
  
 		autoPlay: 3000, //Set AutoPlay to 3 seconds
 			 
-		items : 4,
+		items : 3,
 		itemsDesktop : [1199,3],
 		itemsDesktopSmall : [979,3]
 		 
